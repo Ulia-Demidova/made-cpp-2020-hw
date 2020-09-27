20e81993e529391c74abd38d995068a6f513bbbc
Демидова Юлия <yuliya@MacBook-Pro-Demidova.local>
2020-09-27 03:33:16 +0300

    add solution to main.s

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,34 @@
-/// Your code goes here...
+.text
+.global count_det
 
+count_det:
+        ldr r1, [r0], #4
+        ldr r2, [r0], #4
+        ldr r3, [r0], #4
+        ldr r4, [r0], #4
+        ldr r5, [r0], #4
+        ldr r6, [r0], #4
+        ldr r7, [r0], #4
+        ldr r8, [r0], #4
+        ldr r9, [r0], #4
+
+        mul r10, r5, r1
+        mul r10, r9, r10
+        mul r1, r6, r1
+        mul r1, r8, r1
+        mul r9, r4, r9
+        mul r9, r2, r9
+        mul r2, r6, r2
+        mul r2, r7, r2
+        mul r4, r4, r5
+        mul r4, r4, r8
+        mul r3, r3, r5
+        mul r3, r3, r7
+
+        sub r0, r10, r1
+        sub r0, r0, r9
+        add r0, r2
+        add r0, r4
+        sub r0, r0, r3
+
+        bx lr
